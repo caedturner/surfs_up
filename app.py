06dc2dbfdcf0ce1537d533054746155fc31a9352
Christian Caed Turner <caed@Christians-MacBook-Pro.local>
2022-08-12 12:10:07 -0700

    created and tested app.py.

---
+++ app.py
@@ -0,0 +1,10 @@
+# import Flask
+from flask import Flask
+
+# Create new Flask app instance.
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
